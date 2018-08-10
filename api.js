c5d40fc4df3f966d4345f811143396f2a968a69f
Amy De Genaro <amy.degenaro@gmail.com>
2018-08-10 18:44:38 -0400

    feat(docker): added comments to api routes and helper functions, and created new home component to send for GET requets

--- api.js
+++ api.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const exec = require('child_process').exec;
 const fs = require('fs');
+const Home = require('./home');
 
 const PORT = process.env.PORT || 8080;
 
@@ -15,8 +16,9 @@ app.use(bodyParser.json());
 
 function runUserCode(testFile) {
   return new Promise((resolve, reject) => {
-    exec(`mocha ${testFile}`, { timeout: 10000 }, (err, stdout, stderr) => {
+    exec(`mocha ${testFile}`, { timeout: 5000 }, (err, stdout, stderr) => {
       // delete new test file after running?
+      // where do we add Docker build and run commands?
       if (err !== null && stdout === '') {
         const output = stderr !== '' ? stderr : 'Your code timed out.';
         reject(output);
@@ -26,7 +28,7 @@ function runUserCode(testFile) {
   });
 }
 
-function buildTestFile(code) {
+function buildTestFile(code, specsFile) {
   code = `${code}\n`;
   // remove try catch blocks?
   try {
@@ -35,12 +37,13 @@ function buildTestFile(code) {
       if (err) throw err;
       console.log('new file success!');
     });
+    // eventually comment out below?
     const specs = fs.readFileSync('askPolitely.spec.js', (err, data) => {
       if (err) throw err;
       console.log('read file success!');
       return data;
     });
-    fs.appendFileSync('testMe.js', specs, err => {
+    fs.appendFileSync('testMe.js', specsFile, err => {
       if (err) throw err;
       console.log('append file success!');
     });
@@ -51,11 +54,12 @@ function buildTestFile(code) {
 }
 
 app.get('/', (req, res) => {
-  res.send('This is a placeholder for a really awesome GI-JIF');
+  res.send(Home());
 });
 
 app.post('/', async (req, res, next) => {
   try {
+    // req.body.specs also passed in below
     const testFile = await buildTestFile(req.body.code);
     const result = await runUserCode(testFile);
     res.send(result);
---
+++ home.js
@@ -0,0 +1,9 @@
+module.exports = () => `<!DOCTYPE html>
+  <html>
+    <head>
+      <title>DockerAPI</title>
+    </head>
+    <body>
+      <img src="https://i.imgur.com/6pXxxyZ.gif" alt="animated"/>
+    </body>
+  </html>`;
