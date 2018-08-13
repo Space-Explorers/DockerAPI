e40f35490e41fdeeb3f72cc39367b646cdc84286
DC-1680 <dbconcha@gmail.com>
2018-08-13 17:13:31 -0400

    Update(api.js): Mocha test now creates report instead of string

--- api.js
+++ api.js
@@ -14,18 +14,6 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-function runUserCode(testFile) {
-  return new Promise((resolve, reject) => {
-    exec(`mocha ${testFile}`, { timeout: 10000 }, (err, stdout, stderr) => {
-      if (err !== null && stdout === '') {
-        const output = stderr !== '' ? stderr : 'Your code timed out.';
-        reject(output);
-      }
-      resolve(stdout);
-    });
-  });
-}
-
 function buildTestFile(code, specData) {
   const specs = Buffer.from(specData)
   code = `${code}\n`;
@@ -39,6 +27,30 @@ function buildTestFile(code, specData) {
   return fileName;
 }
 
+function runUserCode(testFile) {
+  return new Promise((resolve, reject) => {
+    exec(`mocha ${testFile} --reporter mochawesome`, { timeout: 10000 }, (err, stdout, stderr) => {
+      if (err !== null && stdout === '') {
+        const output = stderr !== '' ? stderr : 'Your code timed out.';
+        reject(output);
+      }
+      resolve(stdout);
+    });
+  });
+}
+
+function readResultsFile() {
+  try {
+    const resultJSON = fs.readFileSync('mochawesome-report/mochawesome.json', (err, data) => {
+      if (err) throw err
+      return data
+    })
+    return resultJSON;
+  } catch (err) {
+    console.error(err)
+  }
+}
+
 app.get('/', (req, res) => {
   res.send(Home());
 });
@@ -46,8 +58,9 @@ app.get('/', (req, res) => {
 app.post('/', async (req, res, next) => {
   try {
     const testFile = await buildTestFile(req.body.code, req.body.specs.data);
-    const result = await runUserCode(testFile);
-    res.send(result);
+    await runUserCode(testFile);
+    const resultJSON = await readResultsFile();
+    res.json(resultJSON);
   } catch (err) {
     res.json(err);
   }
